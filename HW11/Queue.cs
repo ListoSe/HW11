24b5c726d9d20cde985a6886bfe473fc5b1f3487
Sergii Chernyshov <atiska200612@gmail.com>
2023-04-20 20:47:37 +0300

    Добавьте файлы проекта.

---
+++ HW11/Queue.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW11
+{
+    class Queue<T>
+    {
+        private T[] queue;
+        public void enqueue(T value)
+        {
+            if (queue != null)
+            {
+                var arr = new T[queue.Length+1];
+                for (int i = 0; i < queue.Length; i++)
+                {
+                    arr[i] = queue[i];
+                }
+                arr[arr.Length - 1] = value;
+            }
+            else
+            {
+                queue = new T[1];
+                queue[0] = value;
+            }
+        }
+        public T dequeue()
+        {
+            if (queue != null)
+            {
+                var a = queue[0];
+                var arr = new T[queue.Length-1];
+                for(int i = 1;i < arr.Length;i++){
+                    arr[i-1] = queue[i];
+                }
+                queue = arr;
+                return a;
+            }
+            else
+            {
+                throw new InvalidOperationException("No queue");
+            }
+        }
+        
+        public T peek()
+        {
+            if (queue != null)
+            {
+                return queue[0];
+            }
+            else
+            {
+                throw new InvalidOperationException("No queue");
+            }
+        }
+
+        public int count()
+        {
+            return queue.Length;
+        }
+    }
+}
---
+++ HW11/doubleLinkedList.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace HW11
+{
+    class Node<T>
+    {
+        public T info { get; set; }
+        public Node<T>? prev { get; set; }
+        public Node<T>? next { get; set; }
+
+        public Node(T Info) { info = Info; prev = next = null; }
+    }
+    class doubleLinkedList<T>
+    {
+        private Node<T>? head { get; set; }
+        private Node<T>? current { get; set; }
+
+        public void addLast(T value)
+        {
+            Node<T> el = new Node<T>(value);
+            if (head == null)
+            {
+                head = el; current = el;
+            }
+            else
+            {
+                current.next = el;
+                el.prev = current;
+                current = el;
+            }
+        }
+        public void remove(T val) 
+        {
+            Node<T> tmp = head;
+            while (tmp != null) 
+            {
+                if (tmp.info.Equals(val))
+                {
+                    if (tmp == head)
+                    {
+                        tmp.next.prev = null;
+                        head = tmp.next;
+                        break;
+                    }
+                    else if(tmp == current)
+                    {
+                        tmp.prev.next = null;
+                        current = tmp.prev;
+                        break;
+                    }
+                    else
+                    {
+                        tmp.prev.next = tmp.next;
+                        tmp.next.prev = tmp.prev;
+                        break;
+                    }
+                }
+                else
+                {
+                    tmp = tmp.next;
+                }
+            }
+        }
+        public int count()
+        {
+            int count = 0;
+            Node<T> tmp = head;
+            while (tmp != null)
+            {
+                count++;
+                tmp = tmp.next;
+            }
+            return count;
+        }
+    }
+}
